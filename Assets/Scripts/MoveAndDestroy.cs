5f0d39a1f389379cdcfcd5ce8a09bd3c261b4263
iNatashka99 <iNatashka99@yandex.ru>
2022-12-06 04:48:14 +0300

    go

---
+++ Assets/Scripts/MoveAndDestroy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveAndDestroy : MonoBehaviour
+{
+    public Vector3 start_point;
+    public float min, max;
+    public float speed;
+    public float distance;
+    Vector3 target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerPrefs.SetInt("Pod_count", PlayerPrefs.GetInt("Pod_count") + 1);
+        int t = PlayerPrefs.GetInt("Pod_count");
+        transform.position = new Vector3(PlayerPrefs.GetFloat("startX") + Random.Range(3, 10), start_point.y, start_point.z);
+        target = transform.position;
+        target.x = start_point.x - distance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerPrefs.SetFloat("startX", transform.position.x);
+        transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * speed);
+        if (transform.position.x <= target.x)
+        {
+            Destroy(gameObject);
+            PlayerPrefs.SetInt("Pod_count", PlayerPrefs.GetInt("Pod_count") - 1);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerCntrl.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCntrl : MonoBehaviour
+{
+    public int pod_count;
+    public float jump;
+    public bool moveup;
+    private Vector2 force;
+    private Rigidbody rb;
+    private bool inAir = false;
+    public GameObject podushka;
+    Vector3 pos = new Vector3(100, 0, 0);
+    Quaternion rot = new Quaternion(0, 0, 0, 0);
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        transform.position = new Vector3(0, 0, 0);
+        PlayerPrefs.SetString("finish", "no");
+        PlayerPrefs.SetInt("Pod_count", 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        force = new Vector2(0, jump);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            MoveU();
+        }
+        if (moveup)
+        {
+            MoveU();
+        }
+        int t = PlayerPrefs.GetInt("Pod_count");
+        if (PlayerPrefs.GetInt("Pod_count") < pod_count)
+            Instantiate(podushka, pos, rot);
+
+
+
+    }
+    void MoveU()
+    {
+        if (!inAir)
+        {
+            inAir = true;
+            rb.AddForce(force);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+            inAir = false;
+ 
+    }
+}
